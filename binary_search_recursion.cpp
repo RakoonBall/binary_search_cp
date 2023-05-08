97288dbd9f003689b53c37834fd6d4d87ed4f3a9
Mr. X <mr.x@Mrs-MacBook-Air.local>
2023-05-09 00:57:25 +0600

    add

--- binary_search_recursion.cpp
+++ binary_search_recursion.cpp
@@ -1,7 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int bin_search(int arr[], int s,int e, int k){
+    //base case
+    if(s>e){  //e-s<1
+        return -1;
+    }
+    //calculate mid 
+    int mid = s + (e-s)/2;
+    //recursive calls
+    if(arr[mid]==k){
+        return mid;
+    }else if(arr[mid]> k){
+        e = mid-1;
+        return bin_search(arr, s, e, k);
+    }else{
+        s = mid +1;
+        return bin_search(arr, s, e, k);
+    }
+}
+
 int main(){
-    
+    int arr[6] = {1,2,4,5,6,8};
+    int result = bin_search(arr, 0, 6-1, 5);
+    cout <<"index is "<<  result <<endl;
     return 0;
 }
