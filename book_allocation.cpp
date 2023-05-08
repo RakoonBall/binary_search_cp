f61dcbce56fa0c8636e26acb0929a18a59aa8cd7
Mr. X <mr.x@Mrs-MacBook-Air.local>
2023-05-08 23:27:37 +0600

    add

---
+++ book_allocation.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+//students are m
+int main(int agrc, char *argv[]){
+    
+
+    return 0;
+}
--- square_root_bs.cpp
+++ square_root_bs.cpp
@@ -1,9 +1,31 @@
 #include <iostream>
 using namespace std;
 
+int binary_search(int n){
+    int s = 0;
+    int e = n;
+    int mid = s+ (e-s)/2;
+
+    int ans = -1;
+    while(s<=e){
+        int square = mid*mid;
+        if(square == n){
+            return mid;
+        }
+        if(square < n){
+            ans = mid;
+            s = mid+ 1;
+        }else{
+            e = mid + 1;
+        }
+        mid = s + (e-s)/2;
+        return ans;
+    }
+}
+
 int main(int agrc, char *argv[]){
     int n;
     cin>>n;
-
+    cout<<n;
     return 0;
 }
